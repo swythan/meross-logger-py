7567a2af1aa6b0ce3d2051e0a1603af2c5bbd68e
James Chaldecott <james@swythan.co.uk>
2020-02-07 00:49:31 +0000

    Allow logging multiple devices in one function

--- LogMerossDataTimer/__init__.py
+++ LogMerossDataTimer/__init__.py
@@ -14,6 +14,41 @@ from meross_iot.cloud.devices.power_plugs import GenericPlug
 from meross_iot.manager import MerossManager
 from meross_iot.meross_event import MerossEventType
 
+async def log_device_usage(plug_device: GenericPlug) -> None:
+    device_uuid = plug_device.uuid
+
+    # The connection string for a device should never be stored in code. For the sake of simplicity we're using an environment variable here.
+    iothub_conn_str = os.getenv("IOTHUB_DEVICE_CONNSTR_" + device_uuid)
+
+    if not iothub_conn_str:
+        logging.warn("The plug '%s' (%s) is not configured." % (device_uuid, plug_device.name))
+        return
+
+    # The client object is used to interact with your Azure IoT hub.
+    device_client = IoTHubDeviceClient.create_from_connection_string(iothub_conn_str)
+
+    # Connect the IOT hub client.
+    await device_client.connect()
+
+    logging.info("Smart plug %s" % plug_device.name)
+    logging.info("  uuid : %s" % plug_device.uuid)
+
+    electricity = plug_device.get_electricity()
+
+    converted = {}
+    converted["voltage"] = electricity["voltage"] * 0.1
+    converted["current"] = electricity["current"] * 0.001
+    converted["power"] = electricity["power"] * 0.001
+
+    logging.debug("sending message")
+    msg = Message(str(converted))
+    msg.message_id = uuid.uuid4()
+
+    await device_client.send_message(msg)
+    logging.debug("done sending message")
+
+    # Disconnect IOT hub
+    await device_client.disconnect()
 
 async def main(mytimer: func.TimerRequest) -> None:
     if mytimer.past_due:
@@ -26,11 +61,6 @@ async def main(mytimer: func.TimerRequest) -> None:
 
     meross_email = os.environ.get('MEROSS_EMAIL')
     meross_password = os.environ.get('MEROSS_PASSWORD')
-    meross_device_name = os.environ.get('MEROSS_DEVICE_NAME')
-    meross_device_uuid = os.environ.get('MEROSS_DEVICE_UUID')
-
-    # The connection string for a device should never be stored in code. For the sake of simplicity we're using an environment variable here.
-    iothub_conn_str = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
 
     # Initiates the Meross Cloud Manager. This is in charge of handling the communication with the remote endpoint
     manager = MerossManager(meross_email=meross_email, meross_password=meross_password)
@@ -38,46 +68,20 @@ async def main(mytimer: func.TimerRequest) -> None:
     # Starts the manager
     manager.start()
 
-    # The client object is used to interact with your Azure IoT hub.
-    device_client = IoTHubDeviceClient.create_from_connection_string(iothub_conn_str)
+    plugs = manager.get_devices_by_kind(GenericPlug)
 
-    # Connect the IOT hub client.
-    await device_client.connect()
-
-#    plug_device = manager.get_device_by_name(meross_device_name)
-    plug_device = manager.get_device_by_uuid(meross_device_uuid)
-    
-    if not plug_device:
-        # logging.error("The plug %s is not registered." % meross_device_name)
-        logging.error("The plug %s is not registered." % meross_device_uuid)
-    else:
+    for plug_device in plugs:  # type: GenericPlug
         if not plug_device.online:
             logging.warn("The plug %s seems to be offline." % plug_device.name)
-        else:
-            logging.info("Smart plug %s" % plug_device.name)
-            logging.info("  uuid : %s" % plug_device.uuid)
+            continue
 
-            if not plug_device.supports_electricity_reading():
-                logging.warn("The plug %s does not support power consumption reading." % plug_device.name)
-            else:
-                electricity = plug_device.get_electricity()
+        if not plug_device.supports_electricity_reading():
+            logging.warn("The plug %s does not support power consumption reading." % plug_device.name)
+            continue
 
-                converted = {}
-                converted["voltage"] = electricity["voltage"] * 0.1
-                converted["current"] = electricity["current"] * 0.001
-                converted["power"] = electricity["power"] * 0.001
-
-                logging.debug("sending message")
-                msg = Message(str(converted))
-                msg.message_id = uuid.uuid4()
-
-                await device_client.send_message(msg)
-                logging.debug("done sending message")
+        await log_device_usage(plug_device)
 
     # Disconnect Meross
     manager.stop()
     
-    # Disconnect IOT hub
-    await device_client.disconnect()
-
     logging.info("Function complete")
